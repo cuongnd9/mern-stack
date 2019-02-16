d01d402763b495b51d809bf5600bab2f02cee2dd
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-02-17 00:31:32 +0700

    🌱 initial

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Simple MERN Stack
+      </div>
+    );
+  }
+}
+
+export default App;
